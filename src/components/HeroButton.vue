dfd685753fbecf7943638d14f9dfdd9abd744ab2
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-22 19:15:59 -0300

    create hero section

---
+++ src/components/HeroButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <NavItem 
+        :routeTo="routeTo" 
+        :whereTo="whereTo"
+        class="btn hero-btn w-100"
+    />
+</template>
+
+<script>
+    import NavItem from './NavItem.vue';
+
+    export default {
+        components: {
+            NavItem
+        }, 
+        props: {
+            routeTo: String, 
+            whereTo: String
+        }
+    }
+    
+</script>
+
+<style scoped>
+
+    .hero-btn {
+        background-color: var(--colorBoton);
+        border: 2px solid var(--colorBoton);
+        padding: 1.25rem 2rem;
+        font-size: x-large;
+        font-weight: 700;
+    }
+
+    .hero-btn:hover {
+        background-color: var(--colorPrimario);
+        border: 2px solid var(--colorBoton);
+        color: var(--colorBoton);
+    }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,74 @@
 <template>
-  <!-- <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div> -->
+    <section class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-7 d-flex flex-column mb-5">
+                    <h1><img class="img-titulo w-100" src="../assets/elperrodemessi.png" alt="elperrodemessi-titulo"></h1>
+                    <h2 class="descripcion-index mt-5">Hace más de 30 años nos dedicamos al cuidado de las mascotas. Las mascotas son nuestra pasión. Trabajamos todos los días para mejorar su calidad de vida, como así también la relación con sus tutores mediante el traspaso de información útil y la recomendación de productos adecuados.</h2>
+                    <nav>
+                        <ul class="navbar-nav">
+                            <HeroButton 
+                                routeTo="juguetes" 
+                                whereTo="Premiá a tu mascota!" 
+                                class="my-3" 
+                            />
+                            <HeroButton 
+                                routeTo="farmacia" 
+                                whereTo="Cuidá su salud!"
+                            />
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-md-6 col-lg-5 img-beagle">
+                    <img src="../assets/beagle.png" alt="Beagle cartoon">
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+    import HeroButton from '@/components/HeroButton.vue';
 
-// export default {
-//   name: 'HomeView',
-//   components: {
-//     HelloWorld
-//   }
-// }
+    export default {
+        components: {
+            HeroButton
+        }
+    }
 </script>
 
 <style>
+    .hero {
+        background-color: var(--colorPrimario);
+    }
+
+    h1, 
+    h3 {
+        font-family: 'Holtwood One SC', serif;
+    }
+
+    h2 {
+        font-size: 1.5rem;
+        font-weight: 400;
+    }
+
+    .img-beagle img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .descripcion-index {
+        font-weight: 300;
+        font-size: 1.25rem;
+        line-height: 1.5;
+        color: #fff;
+    }
+
+    .descripcion-index a {
+        color: var(--claro);
+        transition: .3s ease all;
+    }
+
 
 </style>
